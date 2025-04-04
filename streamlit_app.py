52a435a12624b3eee3bd4821e22726b8007a5972
Lucahubgit <lucaandri2000@gmail.com>
2025-04-04 10:16:08 +0000

    Survey

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,49 @@
 import streamlit as st
-import pandas as pd
-import math
-from pathlib import Path
+from datetime import datetime
 
-# Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
+    page_title="Assignment 2B, DV4S",
+    layout="wide",
+    initial_sidebar_state="expanded"
     )
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+st.markdown("""
+    <style>
+    /* Set background color for the entire page */
+    .stApp {
+        background-color: #f0f8ff; /* Light blue background */
+    }
+    /* Set text color for text of the entire app */
+    .stApp {
+        color: #00008b; /* Dark blue color */
+    }
+    </style>
+""", unsafe_allow_html=True)
 
-    return gdp_df
+st.title("Satisfaction survey")
 
-gdp_df = get_gdp_data()
+st.subheader('Please fill in the form below')
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+with st.form('User information'):
+    user_name=st.text_input('Name:',placeholder='Write your name here')
+    rating=st.slider('Select a satisfaction value: ', 1, 5, 3)
+    st.write('Selected value: ', rating)
+    comments=st.text_area('Additional comments:',placeholder='Feel free to write what you want')
+    submitted=st.form_submit_button('Submit')
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
+if submitted:
+    if not user_name.strip():  # Check if the Name field is empty
+        st.error('The "Name" field is mandatory. Please fill it in.')
+    else:
+        st.write('**Form submitted with the following information:**  \n'
+                f'Name: {user_name}  \n'
+                f'Rating: {rating}  \n'
+                f'Comments: {comments}')
+        if rating<3:
+            st.markdown('<p style="color:red; font-size:18px;">'
+                        '<b>Warning!</b> Rating value is lower than 3</p>',
+                        unsafe_allow_html=True)
         else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+            st.markdown('<p style="color:green; font-size:18px;">'
+                        '<b>Success!</b> Rating value is higher than 3</p>',
+                        unsafe_allow_html=True)
